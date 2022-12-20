68da6efabc0aa929cf69a3692e61b3d2ba352a68
applTv <73424904+applTv@users.noreply.github.com>
2022-12-20 14:20:06 +0300

    Add files via upload

---
+++ HappyKul/HappyKul/HappyKulApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyKulApp.swift
+//  HappyKul
+//
+//  Created by Кулешова Анна Викторовна on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct HappyKulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HappyKul/HappyKul/Mine.swift
@@ -0,0 +1,8 @@
+//
+//  Mine.swift
+//  HappyKul
+//
+//  Created by Кулешова Анна Викторовна on 20/12/2022.
+//
+
+import Foundation
